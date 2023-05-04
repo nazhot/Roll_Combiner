3943e22c16ffb9a1094a5811beb7aa70ab4f08ee
nazhot <15216154+nazhot@users.noreply.github.com>
2023-05-04 12:56:07 -0600

    Initial Commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Roll {
+    int number;
+    char* id;
+    float length;
+};
+
+
+int main( int argc, char** args ) {
+    int numberOfRolls = 3;
+    int maxSplices = 4;
+    int maxRollsPerGroups = 4;
+
+
+
+
+    return 0;
+}
