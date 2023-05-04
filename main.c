6f3dcec260a80b59ae4f3fd241cc187f86717f1f
nazhot <15216154+nazhot@users.noreply.github.com>
2023-05-04 13:35:03 -0600

    Set Up File Import
    Got the general skeleton for loading and reading a file set up, same as
    the ydkStats one. No code is being executed.

--- main.c
+++ main.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct Roll {
-    int number;
     char* id;
     float length;
 };
 
 
-int main( int argc, char** args ) {
+int main( int argc, char* argv[] ) {
+
+    if ( argc != 2 ) {
+        printf( "Not the correct number of arguments (1 expected)\n" );
+        return 1;
+    }
+
+    FILE *p_rollFile;
+    const char *fileName = argv[1];
+
+    p_rollFile = fopen( fileName, "r" );
+
+    if ( p_rollFile == NULL ) {
+        printf( "Not able to find file: %s\n", fileName );
+        return 2;
+    }
+
     int numberOfRolls = 3;
     int maxSplices = 4;
     int maxRollsPerGroups = 4;
+    int maxFileLineLength = 100;
+    struct Roll Rolls[numberOfRolls];
 
+    char fileLine[maxFileLineLength];
 
+    while ( fgets( fileLine, maxFileLineLength, p_rollFile ) ) {
+
+    }
 
 
     return 0;
