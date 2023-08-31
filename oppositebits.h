25148a34614cda6faf26b73153837987e02e98ed
nazhot <15216154+nazhot@users.noreply.github.com>
2023-08-30 18:23:13 -0600

    Added Structs and Functions to OB Helper

--- oppositebits.h
+++ oppositebits.h
@@ -1,10 +1,15 @@
 #ifndef OPPOSITE_BITS_
 #define OPPOSITE_BITS_
 
+struct oppositeBits { 
+    int numBits;
+    int ***array;
+};
 
 
-
-
-
+struct oppositeBits* makeOppositeBits( int numBits );
+int* ob_makeFirstArray( struct oppositeBits* );
+int* ob_makeSecondArray( struct oppositeBits*, int );
+int* ob_makeThirdArray( struct oppositeBits*, int, int );
 
 #endif
