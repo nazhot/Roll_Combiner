5f1bb28431a220d6c7590e21b89014741405eb57
nazhot <15216154+nazhot@users.noreply.github.com>
2023-09-09 05:25:34 -0600

    Setting Up testMethods Files
    I am currently putting all of the testing code in the main function, and
    it's lead to a lot of spaghetti code and a convoluted main function.
    Pulling all of the testing out of it, and making it's own function.

---
+++ src/testMethods.h
@@ -0,0 +1,8 @@
+#ifndef TEST_METHODS_H_
+#define TEST_METHODS_H_
+
+long numChecksNormalPairs( struct int_array* );
+long numChecksUnbiasedIntArray( struct int_array** );
+long numChecksBiasedIntArray( struct int_array** );
+
+#endif
