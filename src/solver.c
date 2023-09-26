5a9a1b1ff6e7cd843d6bfb26a269f8d43cfd5a91
nazhot <15216154+nazhot@users.noreply.github.com>
2023-09-26 13:41:11 -0600

    Moved nonRecursiveSolve to threadSolve
    It is now able to work through each index, rather than doing it all at
    once.

--- src/solver.c
+++ src/solver.c
@@ -48,6 +48,56 @@ static int stackIsEmpty( struct SolveStack *solveStack ) {
     return solveStack->top == -1;
 }
 
+static void* threadSolve( int startingIndex, int *numFound, int *ordersWithRollBitMask, struct SmallArray *alreadyFound, struct IntArray **groupsWithRoll, struct OrderStats *orderStats, int *ordersWithRoll ) { 
+    const int8_t numberOfRolls = orderStats->numberOfRolls;
+    struct SolveStack *solveStack = createStack( 500000 );
+    pushStack( solveStack, ( struct StackParameters ) { 0, startingIndex - 1 , 0 } );
+
+    while( !stackIsEmpty( solveStack ) ) {
+        struct StackParameters parameters = popStack( solveStack );
+        const unsigned int currentGroup = parameters.currentGroup;
+        const int currentArrayIndex = parameters.currentArrayIndex;
+        const int numGroupsInOrder = parameters.numGroupsInOrder;
+
+        if ( currentGroup & *ordersWithRollBitMask  ) {
+            continue;
+        }
+
+        setSmallArrayValue( alreadyFound, currentGroup );
+
+        if ( numGroupsInOrder >= orderStats->minGroupsPerOrder && __builtin_popcount( currentGroup ) >= orderStats->minRollsPerOrder ) {
+            const float currentLength = rollsLength( currentGroup, orderStats->numberOfRolls, orderStats->rollList );
+            if ( currentLength >= orderStats->minOrderLength && currentLength <= orderStats->maxOrderLength ) {
+                *numFound += 1;
+                for ( int i = 0; i < numberOfRolls; ++i ) {
+                    ordersWithRoll[i] -= ( currentGroup >> i & 1 );
+                    if ( ordersWithRoll[i] == 0 ) {
+                        *ordersWithRollBitMask |= 1 << i; //|= to guard against this happening multiple times, which it will everytime a new order is found when ordersWithRoll[i] is already 0
+                    }
+                }
+                printf( "\rOrders found: %'i/%'i (%.2f%%)", *numFound, orderStats->numberOfPotentialOrders, *numFound * 1.0 / orderStats->numberOfPotentialOrders * 100 );
+                fflush( stdout );
+            }
+            if ( currentLength + orderStats->minGroupLength > orderStats->maxOrderLength ) {
+                continue;
+            }
+        }
+        for ( int i = currentArrayIndex + 1; i < numberOfRolls; ++i ) {
+            if ( currentGroup >> i & 1 || ordersWithRoll[i] == 0 ) {
+                continue;
+            }
+            for ( int j = 0; j < groupsWithRoll[i]->length; ++j ) {
+                if ( currentGroup & groupsWithRoll[i]->content[j] || groupsWithRoll[i]->content[j] & *ordersWithRollBitMask || getSmallArrayValue( alreadyFound, currentGroup | groupsWithRoll[i]->content[j] ) ) { //could include || groupsWithRoll[i]->content[j] & *ordersWithRollBitMask, but this would only happen if that bitmask
+                    continue;                                            //changes during the run, since it is already checked before
+                }
+                pushStack( solveStack, ( struct StackParameters ) { currentGroup | groupsWithRoll[i]->content[j], i, numGroupsInOrder + 1} );
+            }
+        }
+    }
+
+}
+
+
 void nonRecursiveSolve( struct IntArray **groupsWithRoll, struct OrderStats *orderStats, int *ordersWithRoll ) {
 
     int numFound = 0;
@@ -61,46 +111,49 @@ void nonRecursiveSolve( struct IntArray **groupsWithRoll, struct OrderStats *ord
     int threadId;
     pthread_t pThread;
 
-
-    while( !stackIsEmpty( solveStack ) ) {
-        struct StackParameters parameters = popStack( solveStack );
-        const unsigned int currentGroup = parameters.currentGroup;
-        const int currentArrayIndex = parameters.currentArrayIndex;
-        const int numGroupsInOrder = parameters.numGroupsInOrder;
-
-        if ( currentGroup & ordersWithRollBitMask  ) {
-            continue;
-        }
-
-        setSmallArrayValue( alreadyFound, currentGroup );
-
-        if ( numGroupsInOrder >= orderStats->minGroupsPerOrder && __builtin_popcount( currentGroup ) >= orderStats->minRollsPerOrder ) {
-            const float currentLength = rollsLength( currentGroup, orderStats->numberOfRolls, orderStats->rollList );
-            if ( currentLength >= orderStats->minOrderLength && currentLength <= orderStats->maxOrderLength ) {
-                numFound += 1;
-                for ( int i = 0; i < numberOfRolls; ++i ) {
-                    ordersWithRoll[i] -= ( currentGroup >> i & 1 );
-                    if ( ordersWithRoll[i] == 0 ) {
-                        ordersWithRollBitMask |= 1 << i; //|= to guard against this happening multiple times, which it will everytime a new order is found when ordersWithRoll[i] is already 0
-                    }
-                }
-                printf( "\rOrders found: %'i/%'i (%.2f%%)", numFound, orderStats->numberOfPotentialOrders, numFound * 1.0 / orderStats->numberOfPotentialOrders * 100 );
-                fflush( stdout );
-            }
-            if ( currentLength + orderStats->minGroupLength > orderStats->maxOrderLength ) {
-                continue;
-            }
-        }
-        for ( int i = currentArrayIndex + 1; i < numberOfRolls; ++i ) {
-            if ( currentGroup >> i & 1 || ordersWithRoll[i] == 0 ) {
-                continue;
-            }
-            for ( int j = 0; j < groupsWithRoll[i]->length; ++j ) {
-                if ( currentGroup & groupsWithRoll[i]->content[j] || groupsWithRoll[i]->content[j] & ordersWithRollBitMask || getSmallArrayValue( alreadyFound, currentGroup | groupsWithRoll[i]->content[j] ) ) { //could include || groupsWithRoll[i]->content[j] & *ordersWithRollBitMask, but this would only happen if that bitmask
-                    continue;                                            //changes during the run, since it is already checked before
-                }
-                pushStack( solveStack, ( struct StackParameters ) { currentGroup | groupsWithRoll[i]->content[j], i, numGroupsInOrder + 1} );
-            }
-        }
+    for ( int8_t i = 0; i < numberOfRolls; i++ ) {
+        threadSolve( i, &numFound, &ordersWithRollBitMask, alreadyFound, groupsWithRoll, orderStats, ordersWithRoll );
     }
+
+//    while( !stackIsEmpty( solveStack ) ) {
+//        struct StackParameters parameters = popStack( solveStack );
+//        const unsigned int currentGroup = parameters.currentGroup;
+//        const int currentArrayIndex = parameters.currentArrayIndex;
+//        const int numGroupsInOrder = parameters.numGroupsInOrder;
+//
+//        if ( currentGroup & ordersWithRollBitMask  ) {
+//            continue;
+//        }
+//
+//        setSmallArrayValue( alreadyFound, currentGroup );
+//
+//        if ( numGroupsInOrder >= orderStats->minGroupsPerOrder && __builtin_popcount( currentGroup ) >= orderStats->minRollsPerOrder ) {
+//            const float currentLength = rollsLength( currentGroup, orderStats->numberOfRolls, orderStats->rollList );
+//            if ( currentLength >= orderStats->minOrderLength && currentLength <= orderStats->maxOrderLength ) {
+//                numFound += 1;
+//                for ( int i = 0; i < numberOfRolls; ++i ) {
+//                    ordersWithRoll[i] -= ( currentGroup >> i & 1 );
+//                    if ( ordersWithRoll[i] == 0 ) {
+//                        ordersWithRollBitMask |= 1 << i; //|= to guard against this happening multiple times, which it will everytime a new order is found when ordersWithRoll[i] is already 0
+//                    }
+//                }
+//                printf( "\rOrders found: %'i/%'i (%.2f%%)", numFound, orderStats->numberOfPotentialOrders, numFound * 1.0 / orderStats->numberOfPotentialOrders * 100 );
+//                fflush( stdout );
+//            }
+//            if ( currentLength + orderStats->minGroupLength > orderStats->maxOrderLength ) {
+//                continue;
+//            }
+//        }
+//        for ( int i = currentArrayIndex + 1; i < numberOfRolls; ++i ) {
+//            if ( currentGroup >> i & 1 || ordersWithRoll[i] == 0 ) {
+//                continue;
+//            }
+//            for ( int j = 0; j < groupsWithRoll[i]->length; ++j ) {
+//                if ( currentGroup & groupsWithRoll[i]->content[j] || groupsWithRoll[i]->content[j] & ordersWithRollBitMask || getSmallArrayValue( alreadyFound, currentGroup | groupsWithRoll[i]->content[j] ) ) { //could include || groupsWithRoll[i]->content[j] & *ordersWithRollBitMask, but this would only happen if that bitmask
+//                    continue;                                            //changes during the run, since it is already checked before
+//                }
+//                pushStack( solveStack, ( struct StackParameters ) { currentGroup | groupsWithRoll[i]->content[j], i, numGroupsInOrder + 1} );
+//            }
+//        }
+//    }
 }
