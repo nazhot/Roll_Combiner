6a3f0a6df226bf0eec6647e771772ace9ff2b777
nazhot <15216154+nazhot@users.noreply.github.com>
2023-09-27 17:13:29 -0600

    Removing Group Split Thread, Cleaning Up Code
    Removed the code pertaining to the group split, also removed the normal,
    non-threaded version of non-recursive solve.

--- src/solver.c
+++ src/solver.c
@@ -74,8 +74,8 @@ static void* threadSolve( void *args )  {
     struct IntArray **groupsWithRoll = threadArgs->groupsWithRoll;
     struct OrderStats *orderStats = threadArgs->orderStats;
     int *ordersWithRoll = threadArgs->ordersWithRoll;
-    int startingGroupIndex = threadArgs->startingGroupIndex;
-    int endingGroupIndex = threadArgs->endingGroupIndex;
+    const int startingGroupIndex = threadArgs->startingGroupIndex;
+    const int endingGroupIndex = threadArgs->endingGroupIndex;
     const int8_t numberOfRolls = orderStats->numberOfRolls;
     struct SolveStack *solveStack = createStack( 500000 );
 
@@ -130,7 +130,6 @@ static void* threadSolve( void *args )  {
             }
         }
     }
-    printf( "Completed thread %i\n", startingIndex );
     pthread_exit( NULL );
 }
 
@@ -140,9 +139,6 @@ void nonRecursiveSolve( struct IntArray **groupsWithRoll, struct OrderStats *ord
     int alreadyFoundSize = 1 << orderStats->numberOfRolls;
     const int8_t numberOfRolls = orderStats->numberOfRolls;
     struct SmallArray *alreadyFound = createSmallArray( alreadyFoundSize );
-    //struct SolveStack *solveStack = createStack( 5000000 );
-    //pushStack( solveStack, ( struct StackParameters ) { 0, -1, 0 } );
-
 
     if ( pthread_mutex_init( &setSAMutex, NULL ) != 0 ) { 
         puts( "Failed to initialize 'Set SmallArray' mutex" );
@@ -154,83 +150,28 @@ void nonRecursiveSolve( struct IntArray **groupsWithRoll, struct OrderStats *ord
     }
 
     int threadErrorCode;
-    int numGroupsAtOneTime = 10000;
-    pthread_t threadIds[( int ) ceil( orderStats->numberOfGroups * 1.0 / numGroupsAtOneTime ) + numberOfRolls]; //minimum number of threads is numberOfRolls
-                                                                                                                //the actual number should be much closer to the first expression
-                                                                                                                //numberOfRolls is to guard against weight edge cases/configurations
-    int numberOfThreads = 0;
-    int threadIdSize = ( int ) ceil( orderStats->numberOfGroups * 1.0 / numGroupsAtOneTime ) + numberOfRolls;
+    pthread_t threadIds[numberOfRolls]; 
 
-    for ( int8_t i = 0; i < numberOfRolls; i++ ) {
-        //for ( int j = 0; j < ceil( groupsWithRoll[i]->length * 1.0 / numGroupsAtOneTime ); ++j ) {
+    for ( int8_t i = 0; i < numberOfRolls; ++i ) {
+        struct ThreadArgs *threadArgs = malloc( sizeof( struct ThreadArgs ) );
+        threadArgs->firstRoll = i;
+        threadArgs->numFound = &numFound;;
+        threadArgs->ordersWithRollBitMask = &ordersWithRollBitMask;
+        threadArgs->alreadyFound = alreadyFound;
+        threadArgs->groupsWithRoll = groupsWithRoll;
+        threadArgs->orderStats = orderStats;
+        threadArgs->ordersWithRoll = ordersWithRoll;
+        threadArgs->startingGroupIndex = 0; 
+        threadArgs->endingGroupIndex = groupsWithRoll[i]->length; 
 
-
-            //rc = pthread_create( &threadIds[i], 
-            struct ThreadArgs *threadArgs = malloc( sizeof( struct ThreadArgs ) );
-            threadArgs->firstRoll = i;
-            threadArgs->numFound = &numFound;;
-            threadArgs->ordersWithRollBitMask = &ordersWithRollBitMask;
-            threadArgs->alreadyFound = alreadyFound;
-            threadArgs->groupsWithRoll = groupsWithRoll;
-            threadArgs->orderStats = orderStats;
-            threadArgs->ordersWithRoll = ordersWithRoll;
-            threadArgs->startingGroupIndex = 0; //j * numGroupsAtOneTime;
-            threadArgs->endingGroupIndex = groupsWithRoll[i]->length; //fmin( ( j + 1 ) * numGroupsAtOneTime, groupsWithRoll[i]->length );
-
-            threadErrorCode = pthread_create( &threadIds[numberOfThreads++], NULL, threadSolve, ( void * ) threadArgs );
-            if ( threadErrorCode ) {
-                printf("\n ERROR: return code from pthread_create is %d \n", threadErrorCode );
-                exit(1);
-            }
-        //}
+        threadErrorCode = pthread_create( &threadIds[i], NULL, threadSolve, ( void * ) threadArgs );
+        if ( threadErrorCode ) {
+            printf("\n ERROR: return code from pthread_create is %d \n", threadErrorCode );
+            exit(1);
+        }
     }
 
-    printf( "Number of threads: %i, array size: %i\n", numberOfThreads, threadIdSize );
-
-    for ( int i = 0; i < numberOfThreads; ++i ) {
+    for ( int i = 0; i < numberOfRolls; ++i ) {
         pthread_join( threadIds[i], NULL );
     }
-
-
-//    while( !stackIsEmpty( solveStack ) ) {
-//        struct StackParameters parameters = popStack( solveStack );
-//        const unsigned int currentGroup = parameters.currentGroup;
-//        const int currentArrayIndex = parameters.currentArrayIndex;
-//        const int numGroupsInOrder = parameters.numGroupsInOrder;
-//
-//        if ( currentGroup & ordersWithRollBitMask  ) {
-//            continue;
-//        }
-//
-//        setSmallArrayValue( alreadyFound, currentGroup );
-//
-//        if ( numGroupsInOrder >= orderStats->minGroupsPerOrder && __builtin_popcount( currentGroup ) >= orderStats->minRollsPerOrder ) {
-//            const float currentLength = rollsLength( currentGroup, orderStats->numberOfRolls, orderStats->rollList );
-//            if ( currentLength >= orderStats->minOrderLength && currentLength <= orderStats->maxOrderLength ) {
-//                numFound += 1;
-//                for ( int i = 0; i < numberOfRolls; ++i ) {
-//                    ordersWithRoll[i] -= ( currentGroup >> i & 1 );
-//                    if ( ordersWithRoll[i] == 0 ) {
-//                        ordersWithRollBitMask |= 1 << i; //|= to guard against this happening multiple times, which it will everytime a new order is found when ordersWithRoll[i] is already 0
-//                    }
-//                }
-//                printf( "\rOrders found: %'i/%'i (%.2f%%)", numFound, orderStats->numberOfPotentialOrders, numFound * 1.0 / orderStats->numberOfPotentialOrders * 100 );
-//                fflush( stdout );
-//            }
-//            if ( currentLength + orderStats->minGroupLength > orderStats->maxOrderLength ) {
-//                continue;
-//            }
-//        }
-//        for ( int i = currentArrayIndex + 1; i < numberOfRolls; ++i ) {
-//            if ( currentGroup >> i & 1 || ordersWithRoll[i] == 0 ) {
-//                continue;
-//            }
-//            for ( int j = 0; j < groupsWithRoll[i]->length; ++j ) {
-//                if ( currentGroup & groupsWithRoll[i]->content[j] || groupsWithRoll[i]->content[j] & ordersWithRollBitMask || getSmallArrayValue( alreadyFound, currentGroup | groupsWithRoll[i]->content[j] ) ) { //could include || groupsWithRoll[i]->content[j] & *ordersWithRollBitMask, but this would only happen if that bitmask
-//                    continue;                                            //changes during the run, since it is already checked before
-//                }
-//                pushStack( solveStack, ( struct StackParameters ) { currentGroup | groupsWithRoll[i]->content[j], i, numGroupsInOrder + 1} );
-//            }
-//        }
-//    }
 }
