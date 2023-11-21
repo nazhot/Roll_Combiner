77a3190f1c745a99a82b9ecaea41e0f8149ba06a
nazhot <15216154+nazhot@users.noreply.github.com>
2023-11-20 20:16:59 -0700

    Added FileWriter Documentation, Cleaned Up Code
    Documentation was straight forward in the header, c file had a bunch of
    old code commented out, and wasn't formatted in my favorite way.

--- src/fileWriter.c
+++ src/fileWriter.c
@@ -14,20 +14,15 @@ FILE* createOutputFile( char *fileName ) {
 
     fputs( "Id,Length,Number of Groups,Number of Rolls,Order Groups,Remaining Rolls,Average Remaining Roll Length\n", outputFile );
     return outputFile;
-
 }
-
-
-
 
 void writeOrderToFile( FILE *outputFile, unsigned int order, unsigned int *groups, int numGroups, struct OrderStats *orderStats ) {
     fprintf( outputFile, "%u,", order );
     fprintf( outputFile, "%.1f,", rollsLength( order, orderStats->numberOfRolls, orderStats->rollList ) );
     fprintf( outputFile, "%i,", numGroups );
     fprintf( outputFile, "%i,", __builtin_popcount( order ) );
-    
+    fputc( '\"', outputFile ); //double quotes are used to allow for new lines
 
-    fputc( '\"', outputFile );
     for ( int i = 0; i < numGroups; ++i ) {
         unsigned int group = groups[i];
         float groupLength  = rollsLength( group, orderStats->numberOfRolls, orderStats->rollList );
@@ -42,95 +37,23 @@ void writeOrderToFile( FILE *outputFile, unsigned int order, unsigned int *group
     }
     fputs( "\",", outputFile );
 
-
     int numberOfUnusedRolls = orderStats->numberOfRolls - __builtin_popcount( order );
-    float unusedRollsLength = 0;
-
-    fputc( '\"', outputFile );
-    for ( int i = 0; i < orderStats->numberOfRolls; ++i ) {
-        if ( order >> i & 1 ) {
-            continue;
-        }
-        unusedRollsLength += orderStats->rollList[i].length;
-        fprintf( outputFile, "%s: %.1f", orderStats->rollList[i].id, orderStats->rollList[i].length );
-        if ( i < orderStats->numberOfRolls - 2 ) {
-            fputc( '\n', outputFile );
-        }
-    }
-    fputs( "\",", outputFile );
-    if ( numberOfUnusedRolls ) {
-        fprintf( outputFile, "%.1f", unusedRollsLength / numberOfUnusedRolls );
+    if ( numberOfUnusedRolls == 0 ) {
+        fputs( "0\n", outputFile );
     } else {
-        fputc( '0', outputFile );
+        float unusedRollsLength = 0;
+
+        fputc( '\"', outputFile );
+        for ( int i = 0; i < orderStats->numberOfRolls; ++i ) {
+            if ( order >> i & 1 ) {
+                continue;
+            }
+            unusedRollsLength += orderStats->rollList[i].length;
+            fprintf( outputFile, "%s: %.1f", orderStats->rollList[i].id, orderStats->rollList[i].length );
+            if ( i < orderStats->numberOfRolls - 2 ) {
+                fputc( '\n', outputFile );
+            }
+        }
+        fprintf( outputFile, "\",%.1f\n", unusedRollsLength / numberOfUnusedRolls );
     }
-
-
-    //groups
-    //remaining rolls
-    //average remaining roll length
-    //
-    fputc( '\n', outputFile );
 }
-
-
-
-
-
-//void setGroupsString( unsigned int *groups, int numGroups ) {
-//    fputc( '\"', g_outputFile );
-//    for ( int i = 0; i < numGroups; i++ ){
-//        unsigned int group = groups[i];
-//        float groupLength = rollsLength( group );
-//        char groupLengthString[256];
-//        sprintf( groupLengthString, "%.2f", groupLength );
-//
-//        fputs( "-\n", g_outputFile );
-//
-//        for ( int j = 0; j < numberOfRolls; j++ ) {
-//            if ( !(  group >> j & 1  ) ) {
-//               continue; 
-//            }
-//            char rollLength[256];
-//            sprintf( rollLength, "%.2f", rollList[j].length ); 
-//            fputs( rollList[j].id, g_outputFile );
-//            fputs( ": ", g_outputFile );
-//            fputs( rollLength, g_outputFile );
-//            fputc( '\n', g_outputFile );
-//        }
-//        fputs( "-\n", g_outputFile ); 
-//    }
-//    fputc( '\"', g_outputFile );
-//}
-
-//void setRemainingRollsString( unsigned int order ) {
-//    int numRemainingRolls = 0;
-//    float remainingRollsLength = 0;
-//    float average;
-//    fputc( '\"', g_outputFile );
-//    for ( int i = 0; i < numberOfRolls; i++ ) {
-//        if ( order >> i & 1 ) {
-//            continue;
-//        }
-//        numRemainingRolls++;
-//        remainingRollsLength += rollList[i].length;
-//        char rollLength[256];
-//        sprintf( rollLength, "%.2f", rollList[i].length ); 
-//        fputs( rollList[i].id, g_outputFile );
-//        fputs( ": ", g_outputFile );
-//        fputs( rollLength, g_outputFile );
-//        fputc( '\n', g_outputFile );
-//
-//    }
-//    fputs( "\",", g_outputFile );
-//
-//    if ( numRemainingRolls == 0 ) {
-//        average = 0;
-//    } else {
-//        average = remainingRollsLength / numRemainingRolls;
-//    }
-//
-//    char averageLengthRemaining[256];
-//    sprintf( averageLengthRemaining, "%.2f", average);
-//    fputs( averageLengthRemaining, g_outputFile );
-//    fputc( '\n', g_outputFile );
-//}
--- src/fileWriter.h
+++ src/fileWriter.h
@@ -3,7 +3,31 @@
 #include "roll.h"
 #include <stdio.h>
 
+/**
+ * Create and return the standard output file for orders
+ * 
+ * This will also set the column names within the created file, and these are
+ * not modifiable. If for whatever reason the file cannot be created, the
+ * program will exit.
+ * 
+ * @param fileName path for the output file
+ *
+ * @return pointer to the created file
+ */
 FILE* createOutputFile( char *fileName );
+
+/**
+ * Write a specific order as a line in the output file
+ *
+ * No checking of the parameters is done, and the properties/their orders
+ * are written in a predefined way, no way to modify.
+ *
+ * @param outputFile pointer to the output file
+ * @param order      unsigned int representation of a valid order
+ * @param groups     array of groups that make up the order
+ * @param numGroups  how many groups make up the order
+ * @param orderStats contains the numberOfRolls and rollList
+ */
 void writeOrderToFile( FILE *outputFile, unsigned int order, unsigned int *groups, int numGroups, struct OrderStats *orderStats ); 
 
 #endif
